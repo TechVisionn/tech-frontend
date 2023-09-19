a3298ae9903651b0b15b355447aa6eafdd7d292a
camilaffpacheco <64873345+camilaffpacheco@users.noreply.github.com>
2023-09-18 22:36:05 -0300

    feature-5: Plotando polígono no mapa

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Polygon, Popup } from "react-leaflet";
 import { OpenStreetMapProvider, GeoSearchControl } from "leaflet-geosearch";
 
 import "leaflet/dist/leaflet.css";
@@ -17,6 +17,14 @@ const search = new GeoSearchControl({
   autoClose: true
 });
 
+const coordenadasPoligono = [
+  [-23.2237, -45.9009],
+  [-23.2140, -45.9090],
+  [-23.2153, -45.9217],
+  [-23.2243, -45.9264],
+  [-23.2276, -45.9178]
+];
+
 class Mapa extends Component {
   componentDidMount() {
     setTimeout(() => {
@@ -27,8 +35,13 @@ class Mapa extends Component {
   render() {
     return (
       <div id="mapa">
-        <MapContainer center={[-16, -55]} zoom={5} style={{ height: "100vh" }} ref={ref => this.leafletMap = ref}>
+        <MapContainer center={[-21, -49]} zoom={7} style={{ height: "100vh" }} ref={ref => this.leafletMap = ref}>
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <Polygon positions={coordenadasPoligono} color="green" fillColor="darkgreen" weight={2}>
+            <Popup>
+              {coordenadasPoligono}
+            </Popup>
+          </Polygon>
         </MapContainer>
       </div>
     );
