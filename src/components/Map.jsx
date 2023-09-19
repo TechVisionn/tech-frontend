334bd9cbf66d0eacbdbdfb8631924f8dbeaf2052
BryanRibeiro <bryan.ribeiro@fatec.sp.gov.br>
2023-09-18 22:11:41 -0300

    12: inserção do botão de impressão e zoom

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import { OpenStreetMapProvider, GeoSearchControl } from "leaflet-geosearch";
+import L from "leaflet";
 
 import "leaflet/dist/leaflet.css";
 import "leaflet-geosearch/dist/geosearch.css";
+import "leaflet-easyprint";
 import "../App.css";
 
 const search = new GeoSearchControl({
@@ -17,11 +19,52 @@ const search = new GeoSearchControl({
   autoClose: true
 });
 
+const exportPrint = L.easyPrint({
+  title: 'Imprimir Mapa',
+  position: 'topleft',
+  sizeModes: ['A4Landscape'],
+  filename: 'mapa',
+  exportOnly: true,
+  hideControlContainer: true,
+});
+
 class Mapa extends Component {
+  constructor(props) {
+    super(props);
+    this.handleZoomReset = this.handleZoomReset.bind(this);
+  }
+
   componentDidMount() {
     setTimeout(() => {
       this.leafletMap.addControl(search);
+      this.leafletMap.addControl(exportPrint);
+      this.addCustomZoomResetControl(); // Adicione o controle personalizado
     }, 100);
+  }
+
+  addCustomZoomResetControl() {
+    if (!this.customZoomResetControl) {
+      this.customZoomResetControl = L.control({ position: 'topleft' });
+
+      this.customZoomResetControl.onAdd = () => {
+        const container = L.DomUtil.create('div', 'leaflet-bar leaflet-control');
+        const zoomResetButton = L.DomUtil.create('a', 'leaflet-control-zoom-reset leaflet-bar-part', container);
+        
+        zoomResetButton.href = '#';
+        zoomResetButton.title = 'Redefinir Zoom';
+        zoomResetButton.innerHTML = '↻';
+
+        L.DomEvent.on(zoomResetButton, 'click', this.handleZoomReset);
+
+        return container;
+      };
+
+      this.customZoomResetControl.addTo(this.leafletMap);
+    }
+  }
+
+  handleZoomReset() {
+    this.leafletMap.setView([-16, -55], 5); // Define o centro e o nível de zoom desejados
   }
 
   render() {
@@ -36,3 +79,4 @@ class Mapa extends Component {
 }
 
 export default Mapa;
+
